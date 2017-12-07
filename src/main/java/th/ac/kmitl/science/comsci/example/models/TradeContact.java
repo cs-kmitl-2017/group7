16168e10cf24d1f1da1cf6cbf2b2a3a38880060f
MeeSerCle <meesercle@gmail.com>
2017-12-07 19:25:54 +0700

    Create abstract class at TradeContact.java add properties and get set method

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/TradeContact.java
@@ -0,0 +1,15 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+public abstract class TradeContact {
+    
+    private String uriId;
+
+    public String getUriId() {
+        return uriId;
+    }
+
+    public void setUriId(String uriId) {
+        this.uriId = uriId;
+    }
+    
+}
