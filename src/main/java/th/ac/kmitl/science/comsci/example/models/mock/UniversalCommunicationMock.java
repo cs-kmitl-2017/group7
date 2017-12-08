49767368b9b218b71c207348fa5c3a02420ec3e0
MeeSerCle <meesercle@gmail.com>
2017-12-08 22:09:26 +0700

    Rename method in UniversalCommunicationMock from get to generate and convert to sigleton

--- src/main/java/th/ac/kmitl/science/comsci/example/models/mock/UniversalCommunicationMock.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/mock/UniversalCommunicationMock.java
@@ -4,14 +4,39 @@ import th.ac.kmitl.science.comsci.example.models.UniversalCommunication;
 
 public class UniversalCommunicationMock {
     
+    private static UniversalCommunication 
+            universalCommunicationInstanceMock;
+    private static UniversalCommunication
+            newUniversalCommunicationInstanceMock;
+    
+    private UniversalCommunicationMock(){
+        
+    }
+    
     public static String uriId = "uniMail@GGmail.com";
     public static String newUriId = "newMail@GGmail.com";
     
-    public static UniversalCommunication getUniversalCommunication(){
-        return new UniversalCommunication(uriId);
+    public static UniversalCommunication generateUniversalCommunication(){
+        if(universalCommunicationInstanceMock == null){
+            universalCommunicationInstanceMock = 
+                    new UniversalCommunication(uriId);
+            
+            return universalCommunicationInstanceMock;
+        }else{
+            return universalCommunicationInstanceMock;
+        }
+        
     }
     
-    public static UniversalCommunication getNewUniversalCommunication(){
-        return new UniversalCommunication(newUriId);
+    public static UniversalCommunication generateNewUniversalCommunication(){
+        if(newUniversalCommunicationInstanceMock == null){
+            newUniversalCommunicationInstanceMock = 
+                    new UniversalCommunication(newUriId);
+            
+            return newUniversalCommunicationInstanceMock;
+        }else{
+            return newUniversalCommunicationInstanceMock;
+        }
     }
+    
 }
