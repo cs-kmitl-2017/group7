4154c585ddc7bb9aad5ac157b361dcba8a493b19
MeeSerCle <meesercle@gmail.com>
2017-12-08 14:06:44 +0700

    Modified data type and test case

--- src/main/java/th/ac/kmitl/science/comsci/example/models/TradeContactFactory.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/TradeContactFactory.java
@@ -2,8 +2,8 @@ package th.ac.kmitl.science.comsci.example.models;
 
 public class TradeContactFactory {
     
-    public static TradeContact createTraderContact(String uriId){
+    public static UniversalCommunication createTraderContact(String uriId){
         return new UniversalCommunication(uriId);
-    }
+    } 
     
 }
