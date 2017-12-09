39ad714bca8f1b087d2a4b056fbcf17ac09a5a50
antza123 <irxdz@hotmail.com>
2017-12-09 22:25:21 +0700

    Create CountryCodeMapper and run test mock

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/utilities/CountryCodeMapper.java
@@ -0,0 +1,31 @@
+package th.ac.kmitl.science.comsci.example.models.utilities;
+
+public class CountryCodeMapper implements Mapper {
+
+    @Override
+    public int map(String dataToMap) throws NotFoundMappingException {
+
+        switch (dataToMap) {
+            case "เกาหลี":
+                return 82;
+            case "เนเธอร์แลนด์":
+                return 31;
+            case "ไทย":
+                return 66;
+            case "จีน":
+                return 86;
+            case "ญี่ปุ่น":
+                return 81;
+            case "ฝรั่งเศส":
+                return 33;
+            case "ลาว":
+                return 856;
+            case "สวีเดน":
+                return 46;
+            case "สหรัฐอเมริกา":
+                return 684;
+            default:
+                throw new NotFoundMappingException("404 Not found country name to map");
+        }
+    }
+}
