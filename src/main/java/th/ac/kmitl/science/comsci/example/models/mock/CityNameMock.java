dc39d54443e730926d8f386998ddde29ea16d12a
MeeSerCle <meesercle@gmail.com>
2017-12-09 16:30:59 +0700

    Create CityNameMock class for test

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/mock/CityNameMock.java
@@ -0,0 +1,17 @@
+package th.ac.kmitl.science.comsci.example.models.mock;
+
+public class CityNameMock {
+    
+    public static String cityNameLatKrabang = "ลาดกระบัง";
+    public static String cityNamePhayaThai = "พญาไท";
+    public static String cityNameSathon = "สาทร";
+    public static String cityNameLakSi = "หลักสี่";
+    public static String cityNameWatthana = "วัฒนา";
+    
+    public static int cityNameLatKrabangId = 1011;
+    public static int cityNamePhayaThaiId = 1014;
+    public static int cityNameSathonId = 1028;
+    public static int cityNameLakSiId = 1041;
+    public static int cityNameWatthanaId = 1039;
+    
+}
