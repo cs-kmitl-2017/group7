60450a819affd97180c6325a1d043af76dbea01c
MeeSerCle <meesercle@gmail.com>
2017-12-09 21:21:15 +0700

    Modified throws Exception to NotFoundMappingException

--- src/main/java/th/ac/kmitl/science/comsci/example/models/utilities/Mapper.java
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/utilities/Mapper.java
@@ -2,6 +2,6 @@ package th.ac.kmitl.science.comsci.example.models.utilities;
 
 public interface Mapper {
     
-    public int map(String dataForMap) throws Exception;
+    public int map(String name) throws NotFoundMappingException;
     
 }
