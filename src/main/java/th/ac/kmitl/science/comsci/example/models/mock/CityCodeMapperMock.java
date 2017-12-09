cfe3e9f93196e3d4b52d979b5e5a3da55cadf62a
MeeSerCle <meesercle@gmail.com>
2017-12-09 21:25:15 +0700

    Rename from CityNameMock to CityCodeMapperMock

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/mock/CityCodeMapperMock.java
@@ -0,0 +1,17 @@
+package th.ac.kmitl.science.comsci.example.models.mock;
+
+public class CityCodeMapperMock {
+    
+    public static String latKrabang = "ลาดกระบัง";
+    public static String phayaThai = "พญาไท";
+    public static String sathon = "สาทร";
+    public static String lakSi = "หลักสี่";
+    public static String watthana = "วัฒนา";
+    
+    public static int latKrabangId = 1011;
+    public static int phayaThaiId = 1014;
+    public static int sathonId = 1028;
+    public static int lakSiId = 1041;
+    public static int watthanaId = 1039;
+    
+}
