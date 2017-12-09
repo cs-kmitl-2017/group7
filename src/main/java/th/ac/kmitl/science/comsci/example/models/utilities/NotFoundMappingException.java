2aafdb06ec27b122d1c2484a09d8b65971421c37
MeeSerCle <meesercle@gmail.com>
2017-12-09 20:27:55 +0700

    Create NotFoundMappingException class for not found map

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/utilities/NotFoundMappingException.java
@@ -0,0 +1,9 @@
+package th.ac.kmitl.science.comsci.example.models.utilities;
+
+public class NotFoundMappingException extends Exception{
+    
+    public NotFoundMappingException(String message){
+        super(message);
+    }
+    
+}
