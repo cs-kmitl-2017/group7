bb181de73722fd1840bf5b0012ee86c98a73713f
MeeSerCle <meesercle@gmail.com>
2017-12-09 20:26:42 +0700

    Create NameToIdMapper class for call map method

---
+++ src/main/java/th/ac/kmitl/science/comsci/example/models/utilities/NameToIdMapper.java
@@ -0,0 +1,21 @@
+package th.ac.kmitl.science.comsci.example.models.utilities;
+
+public class NameToIdMapper {
+
+    private static CityNameMapper cityNameMapper;
+    
+    private NameToIdMapper() {
+        
+    }
+    
+    public static int mapCityNameToId(String cityName) 
+            throws NotFoundMappingException{
+        if(cityNameMapper == null){
+            cityNameMapper = new CityNameMapper();
+            return cityNameMapper.map(cityName);
+        }else{
+            return cityNameMapper.map(cityName);
+        }
+    }
+    
+}
