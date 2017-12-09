30ee4bcd19778e7d0cd4907e379c9c090c8ddb88
antza123 <irxdz@hotmail.com>
2017-12-09 23:34:33 +0700

    Create CountrySubDivisionCodeMapper and run test

---
+++ src/test/java/th/ac/kmitl/science/comsci/example/models/CountrySubDivisionCodeMapperTest.java
@@ -0,0 +1,20 @@
+package th.ac.kmitl.science.comsci.example.models;
+
+import org.junit.Test;
+import th.ac.kmitl.science.comsci.example.models.utilities.NotFoundMappingException;
+import th.ac.kmitl.science.comsci.example.models.utilities.CountrySubDivisionCodeMapper;
+
+public class CountrySubDivisionCodeMapperTest {
+
+    @Test
+    public void CountrySubDivisionCodeMapperCorrectly() throws NotFoundMappingException {
+        CountrySubDivisionCodeMapper countrySubDivisionCodeMapper
+                = new CountrySubDivisionCodeMapper();
+
+        assert (countrySubDivisionCodeMapper.map("ขอนแก่น") == 6);
+        assert (countrySubDivisionCodeMapper.map("นนทบุรี") == 24);
+        assert (countrySubDivisionCodeMapper.map("พังงา") == 34);
+        assert (countrySubDivisionCodeMapper.map("ราชบุรี") == 51);
+    }
+
+}
